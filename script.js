286531dbef9430095277b0822dc04e9fbf4a91f2
Andy Stuckey <andystuckey@mail.com>
2024-12-28 04:44:11 -0600

    scripting for buttons

---
+++ script.js
@@ -0,0 +1,38 @@
+let buffer = '0';
+const screen = document.querySelector('.screen');
+
+function buttonClick(value) {
+    if(isNaN(parseInt(value))) {
+        handleSymbol(value);
+    } else {
+        handleNumber(value);
+    }
+    rerender();
+}
+
+function handleNumber(number) {
+    if (buffer === '0') {
+        buffer = number;
+    } else {
+        buffer += number;
+    }
+    
+}
+
+function handleSymbol(symbol) {
+    console.log('symbol');
+}
+
+function init() {
+    console.log("hello");
+    document.querySelector ('.calc-buttons')
+    .addEventListener("click", function(event) {
+    buttonClick(event.target.innerText);
+   });
+}
+
+function rerender() {
+    screen.innerText = buffer;
+}
+
+init();
